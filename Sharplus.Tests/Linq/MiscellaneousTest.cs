9bbba11daf8fc47d4eb63373b0aba75c9fce6020
isjsg97 <josago2009@hotmail.es>
2020-11-20 19:46:55 +0100

    Get Max by selector. Get Min by selector.

--- Sharplus.Tests/Linq/MiscellaneousTest.cs
+++ Sharplus.Tests/Linq/MiscellaneousTest.cs
@@ -16,6 +16,42 @@ namespace Sharplus.Tests.Linq
             Assert.Equal(expected, result);
         }
 
+        [Theory]
+        [InlineData(5, new int[] { 1, 2, 3, 4, 5 })]
+        public void MaxBy(int expected, int[] values)
+        {
+            int result = values.MaxBy(x => x);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 5, 5 }, new int[] { 5, 1, 2, 3, 4, 5 })]
+        public void MaxAllBy(int[] expected, int[] values)
+        {
+            var result = values.MaxAllBy(x => x);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(1, new int[] { 1, 2, 3, 4, 5 })]
+        public void MinBy(int expected, int[] values)
+        {
+            int result = values.MinBy(x => x);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 1 }, new int[] { 1, 2, 3, 4, 5, 1 })]
+        public void MinByAll(int[] expected, int[] values)
+        {
+            var result = values.MinAllBy(x => x);
+
+            Assert.Equal(expected, result);
+        }
+
         [Fact]
         public void Dump()
         {
--- Sharplus/Linq/Miscellaneous.cs
+++ Sharplus/Linq/Miscellaneous.cs
@@ -5,7 +5,7 @@ namespace System.Linq
 {
     public static class Miscellaneous
     {
-        public static IEnumerable<T> Concat<T>(this IEnumerable<T> source, params IEnumerable<T>[] collections)
+        public static IEnumerable<TSource> Concat<TSource>(this IEnumerable<TSource> source, params IEnumerable<TSource>[] collections)
         {
             if (source == null) throw Error.ArgumentNull("source");
             if (collections.Any(c => c == null)) throw Error.ArgumentNull("A collection is null");
@@ -14,71 +14,165 @@ namespace System.Linq
             foreach (var collection in collections) foreach (var item in collection) yield return item;
         }
 
-        public static string Dump<T>(this IEnumerable<T> source)
+        public static string Dump<TSource>(this IEnumerable<TSource> source)
         {
             if (source == null) throw Error.ArgumentNull("source");
             return $"[{string.Join(", ", source)}]";
         }
 
-        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
         {
             if (source == null) throw Error.ArgumentNull("source");
 
-            if (source is T[])
+            if (source is TSource[])
             {
-                Array.ForEach((T[])source, action);
+                Array.ForEach((TSource[])source, action);
             }
-            else if (source is List<T>)
+            else if (source is List<TSource>)
             {
-                ((List<T>)source).ForEach(action);
+                ((List<TSource>)source).ForEach(action);
             }
             else
             {
-                foreach (T item in source) action(item);
+                foreach (TSource item in source) action(item);
             }
         }
 
-        public static void ForEach<T>(this IEnumerable<T> source, Action<int, T> action)
+        public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<int, TSource> action)
         {
             int cont = 0;
             source.ForEach(t => { action(cont, t); cont++; });
         }
 
-        public static T GetRandom<T>(this IEnumerable<T> source, Random random, out int index)
+        public static TSource GetRandom<TSource>(this IEnumerable<TSource> source, Random random, out int index)
         {
             if (source == null) throw Error.ArgumentNull("source");
-            T[] sourceArray = source.ToArray();
+            TSource[] sourceArray = source.ToArray();
             if (sourceArray.Length == 0) throw Error.NoElements();
             index = random.Next(0, sourceArray.Length);
             return sourceArray[index];
         }
 
-        public static T GetRandom<T>(this IEnumerable<T> source, out int index)
+        public static TSource GetRandom<TSource>(this IEnumerable<TSource> source, out int index)
         {
             return GetRandom(source, new Random(), out index);
         }
 
-        public static T GetRandom<T>(this IEnumerable<T> source)
+        public static TSource GetRandom<TSource>(this IEnumerable<TSource> source)
         {
             return GetRandom(source, out int index);
         }
 
-        public static IEnumerable<T> Suffle<T>(this IEnumerable<T> source)
+        public static TSource MaxBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector, IComparer<TKey> comparer = default)
+        {
+            return source.MaxAllBy(selector, comparer).First();
+        }
+
+        public static IEnumerable<TSource> MaxAllBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector, IComparer<TKey> comparer = default)
+        {
+            if (source == null) throw Error.ArgumentNull("source");
+            if (selector == null) throw Error.ArgumentNull("selector");
+
+            comparer = comparer ?? Comparer<TKey>.Default;
+            var elements = new List<TSource>();
+
+            using (var iterator = source.GetEnumerator())
+            {
+                if (!iterator.MoveNext())
+                {
+                    throw Error.NoElements();
+                }
+
+                TSource item = iterator.Current;
+                TKey key = selector(item);
+                TKey maxKey = key;
+                elements.Add(item);
+
+                while (iterator.MoveNext())
+                {
+                    item = iterator.Current;
+                    key = selector(item);
+                    int comparation = comparer.Compare(key, maxKey);
+
+                    if (comparation > 0)
+                    {
+                        elements.Clear();
+                        elements.Add(item);
+                        maxKey = key;
+                    }
+                    else if (comparation == 0)
+                    {
+                        elements.Add(item);
+                    }
+                }
+            }
+
+            return elements;
+        }
+
+        public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector, IComparer<TKey> comparer = default)
+        {
+            return source.MinAllBy(selector, comparer).First();
+        }
+
+        public static IEnumerable<TSource> MinAllBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector, IComparer<TKey> comparer = default)
+        {
+            if (source == null) throw Error.ArgumentNull("source");
+            if (selector == null) throw Error.ArgumentNull("selector");
+
+            comparer = comparer ?? Comparer<TKey>.Default;
+            var elements = new List<TSource>();
+
+            using (var iterator = source.GetEnumerator())
+            {
+                if (!iterator.MoveNext())
+                {
+                    throw Error.NoElements();
+                }
+
+                TSource item = iterator.Current;
+                TKey key = selector(item);
+                TKey minKey = key;
+                elements.Add(item);
+
+                while (iterator.MoveNext())
+                {
+                    item = iterator.Current;
+                    key = selector(item);
+                    int comparation = comparer.Compare(key, minKey);
+
+                    if (comparation < 0)
+                    {
+                        elements.Clear();
+                        elements.Add(item);
+                        minKey = key;
+                    }
+                    else if (comparation == 0)
+                    {
+                        elements.Add(item);
+                    }
+                }
+            }
+
+            return elements;
+        }
+
+        public static IEnumerable<TSource> Suffle<TSource>(this IEnumerable<TSource> source)
         {
             return source.Suffle(new Random());
         }
 
-        public static IEnumerable<T> Suffle<T>(this IEnumerable<T> source, Random random)
+        public static IEnumerable<TSource> Suffle<TSource>(this IEnumerable<TSource> source, Random random)
         {
             return source.OrderBy(_ => random.Next());
         }
 
-        public static IEnumerable<T> SymmetricExcept<T>(this IEnumerable<T> source1, IEnumerable<T> source2)
+        public static IEnumerable<TSource> SymmetricExcept<TSource>(this IEnumerable<TSource> source1, IEnumerable<TSource> source2)
         {
             if (source1 == null) throw Error.ArgumentNull("source1");
             if (source2 == null) throw Error.ArgumentNull("source2");
 
-            HashSet<T> hashSet = new HashSet<T>(source1);
+            HashSet<TSource> hashSet = new HashSet<TSource>(source1);
             hashSet.SymmetricExceptWith(source2);
             return hashSet;
         }
