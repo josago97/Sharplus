e77a36e329778a8f340d9247adf140bef34d4504
isjsg97 <josago2009@hotmail.es>
2020-12-14 02:44:49 +0100

    Duplicate streams. Random get next double in a range. Parse and convert normal string to base64.

---
+++ Sharplus/System/MiscellaneousExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Sharplus.System
+{
+    public static class MiscellaneousExtensions
+    {
+        public static Stream Duplicate(this Stream stream)
+        {
+            stream.Position = 0;
+            var copy = new MemoryStream();
+            stream.CopyTo(copy);
+            copy.Position = 0;
+            stream.Position = 0;
+            return copy;
+        }
+
+        public static double NextDouble(this Random random, double min, double max)
+        {
+            return min + random.NextDouble() * (max - min);
+        }
+    }
+}
---
+++ Sharplus/System/StringExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static string ToBase64(this string text)
+        {
+            return ToBase64(text, Encoding.UTF8);
+        }
+
+        public static string ToBase64(this string text, Encoding encoding)
+        {
+            string result = text;
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                byte[] textAsBytes = encoding.GetBytes(text);
+                result = Convert.ToBase64String(textAsBytes);
+            }
+
+            return result;
+        }
+
+        public static bool TryParseBase64(this string text, out string decodedText)
+        {
+            return TryParseBase64(text, Encoding.UTF8, out decodedText);
+        }
+
+        public static bool TryParseBase64(this string text, Encoding encoding, out string decodedText)
+        {
+            bool parsed = false;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                decodedText = text;
+            }
+            else
+            {
+                try
+                {
+                    byte[] textAsBytes = Convert.FromBase64String(text);
+                    decodedText = encoding.GetString(textAsBytes);
+                    parsed = true;
+                }
+                catch (Exception)
+                {
+                    decodedText = null;
+                }
+            }
+
+            return parsed;
+        }
+    }
+}
