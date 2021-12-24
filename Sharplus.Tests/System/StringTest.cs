6a8aa3c131d5f3f759ad66a4e5674bac2879c9fb
isjsg97 <josago2009@hotmail.es>
2021-12-24 02:41:51 +0100

    Documented StringExtensions.

--- Sharplus.Tests/System/StringTest.cs
+++ Sharplus.Tests/System/StringTest.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using Xunit;
 
 namespace Sharplus.Tests.System
 {
     public class StringTest
     {
+        [Theory]
+        [InlineData(false, -1, "hello", new string[] { })]
+        [InlineData(true, 0, "hello", new string[] { "hello" })]
+        [InlineData(true, 1, "hello", new string[] { "a", "llo" })]
+        public void EndsWith(bool expected, int indexExpected, string text, string[] values)
+        {
+            bool result = text.EndsWith(out int index, values);
+            Assert.Equal(expected, result);
+            Assert.Equal(indexExpected, index);
+        }
+
+        [Theory]
+        [InlineData("hello", "hello", "ñ")]
+        [InlineData("a", "á", "")]
+        [InlineData("añ", "añ", "ñ")]
+        [InlineData("añ", "áñ", "ñ")]
+        public void RemoveAccents(string expected, string text, string ignore)
+        {
+            Assert.Equal(expected, text.RemoveAccents(ignore));
+        }
+
+        [Theory]
+        [InlineData(false, -1, "hello", new string[] { })]
+        [InlineData(true, 0, "hello", new string[] { "hello" })]
+        [InlineData(true, 1, "hello", new string[] { "a", "he" })]
+        public void StartsWith(bool expected, int indexExpected, string text, string[] values)
+        {
+            bool result = text.StartsWith(out int index, values);
+            Assert.Equal(expected, result);
+            Assert.Equal(indexExpected, index);
+        }
+
         [Theory]
         [InlineData("", "")]
         [InlineData("A", "a")]
@@ -19,5 +51,34 @@ namespace Sharplus.Tests.System
             Assert.Equal(expected, text.ToTitleCase(onlyFirst));
         }
 
+        public static IEnumerable<object[]> TranslateData => new List<object[]>
+        {
+            new object[] { "hola", "hola", new Dictionary<string, string>() },
+            new object[] { "ola", "hola", new Dictionary<string, string>(){ { "h", "" } } },
+            new object[] { "hola", "hola", new Dictionary<string, string>() { { "x", "" } } },
+            new object[] { "hola", "hola", new Dictionary<string, string>() { { "x", "h" } } },
+            new object[] { "ola", "hola", new Dictionary<string, string>() { { "h", "" } } },
+            new object[] { "la", "hola", new Dictionary<string, string>() { { "h", "" }, { "o", "" } } },
+            new object[] { "holhholh", "holahola", new Dictionary<string, string>() { {"a","h"} } },
+            new object[] { "olholh", "holahola", new Dictionary<string, string>() { { "a", "h" }, { "h", "" } } },
+            new object[] { "--la--la", "holahola", new Dictionary<string, string>() { { "ho", "--" } } },
+            new object[] { "-la-la", "holahola", new Dictionary<string, string>() { { "ho", "-" } } },
+            new object[] { "laholaho", "holahola", new Dictionary<string, string>() { { "ho", "la" }, { "la", "ho" } } },
+            new object[] { "llohellohe", "hellohello", new Dictionary<string, string>() { { "he", "llo" }, { "llo", "he" } } },
+            new object[] { "he", "hello", new Dictionary<string, string>() { { "he", "" }, { "llo", "he" } } },
+            new object[] { "hehe", "hellohello", new Dictionary<string, string>() { { "he", "" }, { "llo", "he" } } },
+            new object[] { "-he", "hello", new Dictionary<string, string>() { { "llo", "he" }, { "he", "-" } } },
+            new object[] { "-he-he", "hellohello", new Dictionary<string, string>() { { "llo", "he" }, { "he", "-" } } },
+            new object[] { "sxdsxd", "asdasd", new Dictionary<string, string>() { { "a", "s" }, { "s", "x" } } },
+            new object[] { "dxdx", "asdasd", new Dictionary<string, string>() { { "as", "d" }, { "d", "x" } } },
+            new object[] { "xx", "asdasd", new Dictionary<string, string>() { { "as", "" }, { "d", "x" } } },
+        };
+
+        [Theory]
+        [MemberData(nameof(TranslateData))]
+        public void Translate(string expected, string text, Dictionary<string, string> replacements)
+        {
+            Assert.Equal(expected, text.Translate(replacements));
+        }
     }
 }
--- Sharplus/System/StringExtensions.cs
+++ Sharplus/System/StringExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text;
 using System.Threading;
 
@@ -8,11 +9,161 @@ namespace System
 {
     public static class StringExtensions
     {
-        public static string ToBase64(this string text)
+        #region EndsWith
+
+        /// <summary>
+        /// Determines if the end of <paramref name="text"/> matches any of the specified strings.
+        /// </summary>        
+        /// <param name="text">The string to compare to the substrings at its end.</param>
+        /// <param name="suffixIndex">The zero-based index of the first occurrence, if found; otherwise, -1.</param>
+        /// <param name="values">The specified strings to compare to the end of <paramref name="text"/>.</param>
+        /// <returns>
+        /// <see langword="true"/> if the end of <paramref name="text"/> matches any of the strings of <paramref name="values"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        public static bool EndsWith(this string text, out int suffixIndex, IEnumerable<string> values)
         {
-            return ToBase64(text, Encoding.UTF8);
+            suffixIndex = values.FindIndex(s => text.EndsWith(s));
+            return suffixIndex >= 0;
         }
 
+        /// <summary>
+        /// Determines if the end of <paramref name="text"/> matches any of the specified strings.
+        /// </summary>        
+        /// <param name="text">The string to compare to the substrings at its end.</param>
+        /// <param name="comparisonType">One of the enumeration values that determines how <paramref name="text"/> and other <see cref="string"/> are compared.</param>
+        /// <param name="suffixIndex">The zero-based index of the first occurrence, if found; otherwise, -1.</param>
+        /// <param name="values">The specified strings to compare to the end of <paramref name="text"/>.</param>
+        /// <returns>
+        /// <see langword="true"/> if the end of <paramref name="text"/> matches any of the strings of <paramref name="values"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        public static bool EndsWith(this string text, StringComparison comparisonType, out int suffixIndex, IEnumerable<string> values)
+        {
+            suffixIndex = values.FindIndex(s => text.EndsWith(s, comparisonType));
+            return suffixIndex >= 0;
+        }
+
+        /// <summary>
+        /// Determines if the end of <paramref name="text"/> matches any of the specified strings.
+        /// </summary>        
+        /// <param name="text">The string to compare to the substrings at its end.</param>
+        /// <param name="ignoreCase"><see langword="true"/> to ignore case during the comparison; otherwise, <see langword="false"/>.</param>
+        /// <param name="culture">
+        /// Cultural information that determines how <paramref name="text"/> and other <see cref="string"/> are compared.
+        /// If culture is null, the current culture is used.
+        /// </param>
+        /// <param name="suffixIndex">The zero-based index of the first occurrence, if found; otherwise, -1.</param>
+        /// <param name="values">The specified strings to compare to the end of <paramref name="text"/>.</param>
+        /// <returns>
+        /// <see langword="true"/> if the end of <paramref name="text"/> matches any of the strings of <paramref name="values"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        public static bool EndsWith(this string text, bool ignoreCase, CultureInfo culture, out int suffixIndex, IEnumerable<string> values)
+        {
+            suffixIndex = values.FindIndex(s => text.EndsWith(s, ignoreCase, culture));
+            return suffixIndex >= 0;
+        }
+
+        #endregion
+
+        #region RemoveAccents
+
+        /// <summary>
+        /// Returns a copy of <paramref name="text"/> without accents.
+        /// </summary>        
+        /// <param name="text">The string to remove specified accents.</param>
+        /// <param name="ignore">The characters with accent to ignore.</param>
+        /// <returns>
+        /// The specified <see cref="string"/> without accents.
+        /// </returns>
+        public static string RemoveAccents(this string text, string ignore = "")
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            IEnumerable<char> textNormalized = text.Normalize(NormalizationForm.FormD)
+                .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark);
+
+            textNormalized.ForEach((i, cn) =>
+            {
+                char c = text[i];
+
+                if (ignore.Contains(c))
+                {
+                    stringBuilder.Append(c);
+                }
+                else
+                {
+                    stringBuilder.Append(cn);
+                }
+            });
+
+            return stringBuilder.ToString();
+        }
+
+        #endregion
+
+        #region StartsWith
+
+        /// <summary>
+        /// Determines if the start of <paramref name="text"/> matches any of the specified strings.
+        /// </summary>        
+        /// <param name="text">The string to compare to the substrings at its start.</param>
+        /// <param name="prefixIndex">The zero-based index of the first occurrence, if found; otherwise, -1.</param>
+        /// <param name="values">The specified strings to compare to the start of <paramref name="text"/>.</param>
+        /// <returns>
+        /// <see langword="true"/> if the start of <paramref name="text"/> matches any of the strings of <paramref name="values"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        public static bool StartsWith(this string text, out int prefixIndex, IEnumerable<string> values)
+        {
+            prefixIndex = values.FindIndex(s => text.StartsWith(s));
+            return prefixIndex >= 0;
+        }
+
+        /// <summary>
+        /// Determines if the start of <paramref name="text"/> matches any of the specified strings.
+        /// </summary>        
+        /// <param name="text">The string to compare to the substrings at its start.</param>
+        /// <param name="comparisonType">One of the enumeration values that determines how <paramref name="text"/> and other <see cref="string"/> are compared.</param>
+        /// <param name="prefixIndex">The zero-based index of the first occurrence, if found; otherwise, -1.</param>
+        /// <param name="values">The specified strings to compare to the start of <paramref name="text"/>.</param>
+        /// <returns>
+        /// <see langword="true"/> if the start of <paramref name="text"/> matches any of the strings of <paramref name="values"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        public static bool StartsWith(this string text, StringComparison comparisonType, out int prefixIndex, IEnumerable<string> values)
+        {
+            prefixIndex = values.FindIndex(s => text.StartsWith(s, comparisonType));
+            return prefixIndex >= 0;
+        }
+
+        /// <summary>
+        /// Determines if the start of <paramref name="text"/> matches any of the specified strings.
+        /// </summary>        
+        /// <param name="text">The string to compare to the substrings at its start.</param>
+        /// <param name="ignoreCase"><see langword="true"/> to ignore case during the comparison; otherwise, <see langword="false"/>.</param>
+        /// <param name="culture">
+        /// Cultural information that determines how <paramref name="text"/> and other <see cref="string"/> are compared.
+        /// If culture is null, the current culture is used.
+        /// </param>
+        /// <param name="prefixIndex">The zero-based index of the first occurrence, if found; otherwise, -1.</param>
+        /// <param name="values">The specified strings to compare to the start of <paramref name="text"/>.</param>
+        /// <returns>
+        /// <see langword="true"/> if the start of <paramref name="text"/> matches any of the strings of <paramref name="values"/>; otherwise, <see langword="false"/>.
+        /// </returns>
+        public static bool StartsWith(this string text, bool ignoreCase, CultureInfo culture, out int prefixIndex, IEnumerable<string> values)
+        {
+            prefixIndex = values.FindIndex(s => text.StartsWith(s, ignoreCase, culture));
+            return prefixIndex >= 0;
+        }
+
+        #endregion
+
+        #region Base64
+
+        /// <summary>
+        /// Returns a copy of <paramref name="text"/> that is encoded with base-64 digits.
+        /// </summary>        
+        /// <param name="text">The string to encoded.</param>
+        /// <param name="encoding">The source encoding of <paramref name="text"/>.</param>
+        /// <returns>
+        /// The specified <see cref="string"/> encoded with base-64 digits.
+        /// </returns>
         public static string ToBase64(this string text, Encoding encoding)
         {
             string result = text;
@@ -26,12 +177,28 @@ namespace System
             return result;
         }
 
-        public static bool TryParseBase64(this string text, out string decodedText)
+        /// <summary>
+        /// Returns a copy of <paramref name="text"/> that is encoded with base-64 digits.
+        /// </summary>        
+        /// <param name="text">The string to encoded.</param>
+        /// <returns>
+        /// The specified <see cref="string"/> encoded with base-64 digits.
+        /// </returns>
+        public static string ToBase64(this string text)
         {
-            return TryParseBase64(text, Encoding.UTF8, out decodedText);
+            return ToBase64(text, Encoding.UTF8);
         }
 
-        public static bool TryParseBase64(this string text, Encoding encoding, out string decodedText)
+        /// <summary>
+        /// Tries to decode a string that is encoded with base-64 digits and encodes it to another specified encoding.
+        /// </summary>        
+        /// <param name="text">The <see cref="string"/> to decoded.</param>
+        /// <param name="encoding">The destination encoding of <paramref name="text"/>.</param>
+        /// <param name="decodedText">If the decoding succeeded, this contains the decoded <see cref="string"/>, or null if the decoding failed.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="text"/> was decoded successfully; otherwise, <see langword="false"/>.
+        /// </returns>
+        public static bool TryFromBase64(this string text, Encoding encoding, out string decodedText)
         {
             bool parsed = false;
 
@@ -56,6 +223,31 @@ namespace System
             return parsed;
         }
 
+        /// <summary>
+        /// Tries to decode a string that is encoded with base-64 digits and encodes it to the default encoding.
+        /// </summary>        
+        /// <param name="text">The <see cref="string"/> to decoded.</param>
+        /// <param name="decodedText">If the decoding succeeded, this contains the decoded <see cref="string"/>, or null if the decoding failed.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="text"/> was decoded successfully; otherwise, <see langword="false"/>.
+        /// </returns>
+        public static bool TryFromBase64(this string text, out string decodedText)
+        {
+            return TryFromBase64(text, Encoding.Default, out decodedText);
+        }
+
+        #endregion
+
+        #region ToTitleCase
+
+        /// <summary>
+        /// Returns a copy of <paramref name="text"/> converted to title case.
+        /// </summary>        
+        /// <param name="text">The string to convert.</param>
+        /// <param name="onlyFirst"><see langword="true"/> to set upper case only the first character; <see langword="false"/> to convert all.</param>
+        /// <returns>
+        /// The specified <see cref="string"/> converted to title case.
+        /// </returns>
         public static string ToTitleCase(this string text, bool onlyFirst = true)
         {
             string result = string.Empty;
@@ -76,9 +268,72 @@ namespace System
             return result;
         }
 
+        /// <summary>
+        /// Returns a copy of <paramref name="text"/> converted to title case (except for words 
+        /// that are entirely in uppercase, which are considered to be acronyms).
+        /// </summary>        
+        /// <param name="text">The string to convert.</param>
+        /// <param name="cultureInfo">The <see cref="CultureInfo"/> that defines the writing system.</param>
+        /// <returns>
+        /// The specified <see cref="string"/> converted to title case.
+        /// </returns>
         public static string ToTitleCase(this string text, CultureInfo cultureInfo)
         {
-            return cultureInfo.TextInfo.ToTitleCase(text.ToLower());
+            return cultureInfo.TextInfo.ToTitleCase(text);
         }
+
+        #endregion
+
+        #region Translate
+
+        /// <summary>
+        /// Returns a copy of <paramref name="text"/> in which all occurrences of all specified replacements are replaced.
+        /// </summary>        
+        /// <param name="text">The string to replace with the specified replacements.</param>
+        /// <param name="replacements">The replacements that define which <see cref="string"/> is replaced by another <see cref="string"/>.</param>
+        /// <returns>
+        /// The specified <see cref="string"/> with all replacements applied.
+        /// </returns>
+        public static string Translate(this string text, Dictionary<string, string> replacements)
+        {
+            if (replacements.Count == 0) return text;
+
+            StringBuilder result = new StringBuilder();
+            StringBuilder pattern = new StringBuilder();
+            int countIndex = 0;
+
+            while (countIndex < text.Length)
+            {
+                bool isReplaced = false;
+
+                for (int i = countIndex; i < text.Length && !isReplaced; i++)
+                {
+                    pattern.Append(text[i]);
+
+                    if (replacements.TryGetValue(pattern.ToString(), out string newValue))
+                    {
+                        result.Append(newValue);
+                        isReplaced = true;
+                    }
+                }
+
+                if (isReplaced)
+                {
+                    countIndex += pattern.Length;
+                }
+                else
+                {
+                    result.Append(text[countIndex]);
+                    countIndex++;
+                }
+
+                pattern.Clear();
+            }
+
+            return result.ToString();
+        }
+
+        #endregion
+
     }
 }
