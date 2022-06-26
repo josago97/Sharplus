eab451cdc9f8cab066405b96e190f53e258a46c7
josago97 <josago2009@hotmail.es>
2022-06-26 18:39:34 +0200

    Load environment variables. Publish package in Nuget v1.1.2.

---
+++ Sharplus/System/EnvironmentUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Sharplus.System
+{
+    public static class EnvironmentUtils
+    {
+        public static void LoadVariables(string filePath)
+        {
+            using Stream file = File.OpenRead(filePath);
+            LoadVariables(file);
+        }
+
+        public static void LoadVariables(Stream stream)
+        {
+            using TextReader reader = new StreamReader(stream);
+
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (!line.StartsWith('#'))
+                {
+                    string[] parts = line.Split('=');
+                    string variable = parts[0].Trim();
+                    string value = parts[1].Trim();
+
+                    Environment.SetEnvironmentVariable(variable, value);
+                }
+            }
+        }
+    }
+}
